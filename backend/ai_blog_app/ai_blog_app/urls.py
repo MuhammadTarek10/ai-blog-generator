f8f77b679dcf3af950c76cb77aadf7188bc26dd3
Muhammad Tarek <mohammadtarek000@gmail.com>
2024-04-14 19:28:01 +0200

    started backend, made signup with sqlite

--- backend/ai_blog_app/ai_blog_app/urls.py
+++ backend/ai_blog_app/ai_blog_app/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-
-from . import views
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path("", views.index),
+    path("admin", admin.site.urls),
+    path('', include('blog_generator.urls'))
 ]
--- backend/ai_blog_app/blog_generator/views.py
+++ backend/ai_blog_app/blog_generator/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
+
 
 # Create your views here.
+def index(request) -> None:
+    return render(request, "index.html")
+
+
+def user_login(request) -> None:
+    return render(request, "login.html")
+
+
+def user_signup(request) -> None:
+    if request.method == "POST":
+        email = request.POST["email"]
+        username = request.POST["username"]
+        password = request.POST["password"]
+        confirmation_password = request.POST["confirmation-password"]
+
+        if password != confirmation_password:
+            return render(
+                request, "signup.html", {"error_message": "Password don't match"}
+            )
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+            login(request, user)
+            return redirect("/")
+        except:
+            return render(
+                request, "signup.html", {"error_message": "Error creating user"}
+            )
+
+    return render(request, "signup.html")
+
+
+def user_logout(request) -> None:
+    pass
